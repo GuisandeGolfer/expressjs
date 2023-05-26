5dfe37abb2d39d7493ceaeb676dfd096adff5a1f
GuisandeGolfer <DGuisande873@gmail.com>
2023-05-26 10:47:32 -0700

    feat(add openAI post route): simple operational typescript version of project

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,15 @@
 import express from 'express';
+import * as dotenv from 'dotenv';
+import { Configuration, OpenAIApi } from 'openai';
 import cors from 'cors';
+
+dotenv.config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI
+});
+
+const openai = new OpenAIApi(configuration);
 
 export const app = express();
 
@@ -10,14 +20,37 @@ app.use(express.raw({ type: 'application/vnd.custom-type' }));
 app.use(express.text({ type: 'text/html' }));
 
 // Healthcheck endpoint
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.status(200).send({ status: 'ok' });
 });
 
 const api = express.Router();
 
-api.get('/hello', (req, res) => {
+api.get('/hello', (_req, res) => {
   res.status(200).send({ message: 'hello world' });
+});
+
+api.get('/dream', (_req, res) => {
+  res.status(200).send({ message: 'hello world' });
+});
+
+app.post('/dream', async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+
+    const aiResponse = await openai.createImage({
+      prompt,
+      n: 1,
+      size: '1024x1024'
+    });
+
+    const image = aiResponse.data.data[0].url;
+    res.send({ image });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('something went wrong.');
+  }
+  // error?.response.data.error.message // optional chaining not working with typescript, mean to be in send().
 });
 
 // Version the api
